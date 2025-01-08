213f09849e3068b0cc4e25c2417c9f6a9fcc15cb
Artem Kotko <artem.kotko@ukr.net>
2025-01-08 21:44:27 +0100

    Working mobile design and showing/ deseappearing when change the screen

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 const authorDiv = document.querySelector(".author");
 const shareDiv = document.querySelector(".share");
 const toggleButtons = document.querySelectorAll(".toggle__btn");
-const shareButton = document.getElementById("share__btn");
-const toggleBtn = document.getElementById("toggle__button");
-
-// Get references to the elements
-const imgElement = document.getElementById("author__avatar");
-const pElement = document.querySelector(".author__data");
-
-// Group them into a single object
-const authorObject = {
-  image: imgElement,
-  text: pElement,
-  button: toggleBtn,
-};
 
 // Add click event listeners to both buttons
 toggleButtons.forEach((button) => {
@@ -21,38 +8,44 @@ toggleButtons.forEach((button) => {
     // Check if the screen width is less than 800px (50rem)
     if (window.innerWidth < 800) {
       // Check which div is currently visible
-      if (shareDiv.style.display == "flex") {
+      if (authorDiv.style.display === "none") {
         // Show the author div and hide the share div
-        authorObject.image.style.display = "flex";
-        authorObject.text.style.display = "flex";
-        authorObject.button.style.display = "flex";
+        authorDiv.style.display = "flex";
         shareDiv.style.display = "none";
       } else {
         // Show the share div and hide the author div
-        authorObject.image.style.display = "none";
-        authorObject.text.style.display = "none";
-        authorObject.button.style.display = "none";
+        authorDiv.style.display = "none";
         shareDiv.style.display = "flex";
-        authorDiv.style.padding = "0";
       }
     } else {
-      shareDiv.style.position = "absolute";
-      toggleBtn.style.position = "relative";
-      authorDiv.style.position = "relative";
-      shareDiv.style.right = "-2rem"; // Align it to the right of the button
-      shareDiv.style.top = "-5rem";
-      shareButton.style.display = "none";
-
-      if (shareDiv.style.display === "none") {
-        // Show the author div and hide the share div
-        shareDiv.style.display = "flex";
-      } else {
-        // Show the share div and hide the author div
-        shareDiv.style.display = "none";
-      }
+      authorDiv.style.display = "flex";
+if(shareDiv.style.display == "none"){
+shareDiv.style.display = "flex";
+}
+else{
+shareDiv.style.display = "none";
+}
     }
   });
 });
+
+function updateShareDiv() {
+  // Check if the screen width is less than 800px (50rem)
+  if (window.innerWidth > 800) {
+    authorDiv.style.display = "flex";
+    
+  } else {
+    if (authorDiv.style.display === "none") {
+      // Show the author div and hide the share div
+      authorDiv.style.display = "none";
+      shareDiv.style.display = "flex";
+    } else {
+      // Show the share div and hide the author div
+      authorDiv.style.display = "flex";
+      shareDiv.style.display = "none";
+    }
+  }
+}
 
 // Initial setup
 updateShareDiv();
