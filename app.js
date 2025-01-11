fd057d42de211417bcd2e6b1aff5a4672be69640
Artem Kotko <artem.kotko@ukr.net>
2025-01-11 22:08:34 +0100

    First commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const authorDiv = document.querySelector(".author");
 const shareDiv = document.querySelector(".share");
 const toggleButtons = document.querySelectorAll(".toggle__btn");
+const image = document.querySelector(".btn__image");
+const authorButton = document.getElementById("author__btn");
+const shareButton = document.getElementById("share__btn");
+const shareImg = document.getElementById("share__img");
+
+
+// Sets the initial display state of the shareDiv to "none" to ensure correct toggling
+shareDiv.style.display = "none";
+
 
 // Add click event listeners to both buttons
 toggleButtons.forEach((button) => {
@@ -12,39 +21,51 @@ toggleButtons.forEach((button) => {
         // Show the author div and hide the share div
         authorDiv.style.display = "flex";
         shareDiv.style.display = "none";
+
       } else {
         // Show the share div and hide the author div
         authorDiv.style.display = "none";
         shareDiv.style.display = "flex";
+        shareButton.style.backgroundColor = "hsl(214, 17%, 51%)";
+        shareImg.style.filter = "invert(100%) brightness(200%)";
       }
     } else {
       authorDiv.style.display = "flex";
 if(shareDiv.style.display == "none"){
 shareDiv.style.display = "flex";
+button.style.backgroundColor = "hsl(214, 17%, 51%)";
+image.style.filter = "invert(100%) brightness(200%)";//This combination ensures that the image becomes white, regardless of its original color
 }
 else{
 shareDiv.style.display = "none";
+image.style.filter = "none"; 
+button.style.backgroundColor = "rgb(240, 240, 240)";
 }
     }
   });
 });
 
 function updateShareDiv() {
-  // Check if the screen width is less than 800px (50rem)
+  
   if (window.innerWidth > 800) {
     authorDiv.style.display = "flex";
     
   } else {
     if (authorDiv.style.display === "none") {
-      // Show the author div and hide the share div
       authorDiv.style.display = "none";
       shareDiv.style.display = "flex";
     } else {
-      // Show the share div and hide the author div
+      image.style.filter = "none"; 
+      authorButton.style.backgroundColor = "rgb(240, 240, 240)";
       authorDiv.style.display = "flex";
       shareDiv.style.display = "none";
     }
   }
+//Keep the color of the button if we change screen size from small to big and share div is visible
+if (window.innerWidth > 800 && shareDiv.style.display == "flex" ) {
+authorButton.style.backgroundColor = "hsl(214, 17%, 51%)";
+image.style.filter = "invert(100%) brightness(200%)";
+}
 }
 
 // Initial setup
